f62a25092a4f3134a71cf518967a3a908f32bf1c
AboobaeNna <108238151+AboobaeNna@users.noreply.github.com>
2022-07-04 07:12:00 +0800

    added a script

---
+++ index.js
@@ -0,0 +1,83 @@
+const gallery = document.querySelectorAll('.pic');
+const heroImg = document.querySelector('.product-hero');
+
+const btnNext = document.querySelector('.next');
+const btnPrevious = document.querySelector('.previous');
+
+const overlay = document.querySelector('.overlay');
+const lightbox = document.querySelector('.lightbox');
+
+let lightboxGallery;
+let lightboxHero;
+
+
+
+
+gallery.forEach(img => {
+    img.addEventListener('click', onThumbClick);
+});
+
+btnNext.addEventListener('click', handleBtnClickNext);
+btnPrevious.addEventListener('click', handleBtnClickPrevious);
+
+
+
+
+
+function onThumbClick(event) {
+    //clear active state for all thumbnails
+    gallery.forEach(img => {
+        img.classList.remove('active');
+    });
+    //set active thumbnail
+    event.target.parentElement.classList.add('active');
+    //update hero image
+    heroImg.src = event.target.src.replace('-thumbnail', '');
+}
+
+function handleBtnClickNext() {
+    let imageIndex = getCurrentImageIndex();
+    imageIndex++;
+    if (imageIndex > 4) {
+        imageIndex = 1;
+    }
+    setHeroImage(imageIndex);
+}
+
+function handleBtnClickPrevious() {
+    let imageIndex = getCurrentImageIndex();
+    imageIndex--;
+    if (imageIndex < 1) {
+        imageIndex = 4;
+    }
+    setHeroImage(imageIndex);
+}
+
+function getCurrentImageIndex() {
+    const imageIndex = parseInt(heroImg.src.split('\\').pop().split('/').pop().replace('.jpg', '').replace('image-product-', ''));
+    return imageIndex;
+}
+
+function setHeroImage(imageIndex) {
+    heroImg.src = `./images/image-product-${imageIndex}.jpg`;
+    //images are not sync
+    gallery.forEach(img => {
+        img.classList.remove('active');
+    });
+    //set active thumbnail
+    gallery[imageIndex-1].classList.add('active');
+}
+
+//popup-image 
+
+document.querySelectorAll('.thumb-gallery img').forEach(Image =>{
+    Image.onclick =() =>{
+document.querySelector('.popup-image').style.display = 'block';
+document.querySelector('.popup-image img').src =Image.getAttribute('src');
+}
+
+});
+
+document.querySelector('.popup-image span').onclick =() =>{
+    document.querySelector('.popup-image').style.display = 'none';
+}
